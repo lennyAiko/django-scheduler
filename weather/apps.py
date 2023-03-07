3be2d7bbe79f68b624ee77a6154d46dff9f33249
Lennox Charles <lennyaiko17@gmail.com>
2023-03-07 17:45:29 +0100

    fixes

--- weather/apps.py
+++ weather/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class WeatherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather'
+
+    def ready(self):
+        print("Starting Scheduler ...")
+        from .scheduler import updater
+        updater.start()
